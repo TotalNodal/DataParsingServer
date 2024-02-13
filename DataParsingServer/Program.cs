ad1e3c58d7375c4469aeda25f98d28fbc95ee63a
TotalNodal <150134390+TotalNodal@users.noreply.github.com>
2024-02-13 10:43:04 +0100

    Add project files.

---
+++ DataParsingServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataParsingServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
